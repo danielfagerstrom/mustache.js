aa293f9c9d5beda452068e6f68a909e2177161b8
Daniel Fagerstrom <daniel@danielfagerstrom.com>
2013-03-13 23:30:37 +0100

    Test rendering using partials functions returning deferreds and partials objects containing deferreds.

--- test/deferred-templates-test.js
+++ test/deferred-templates-test.js
@@ -14,4 +14,26 @@ describe('Mustache.render with deferred templates', function() {
       done();
     }).done();
   });
+
+  it('knows how to render using a deferred partial (object)', function(done) {
+    var template = Q.resolve("-- {{title}} --\n{{>subtemplate}}"),
+       partials = { subtemplate: Q.resolve("* {{content}} *") },
+       view = { title: "Test", content: "A text" };
+    Q.when(Mustache.render(template, view, partials), function(result) {
+      assert.equal(result, "-- Test --\n* A text *");
+      done();
+    }).done();
+  });
+
+  it('knows how to render using a deferred partial (function)', function(done) {
+    var template = Q.resolve("-- {{title}} --\n{{>subtemplate}}"),
+       partials = function(name) {
+         return name === 'subtemplate' ? Q.resolve("* {{content}} *"): null;
+       },
+       view = { title: "Test", content: "A text" };
+    Q.when(Mustache.render(template, view, partials), function(result) {
+      assert.equal(result, "-- Test --\n* A text *");
+      done();
+    }).done();
+  });
 });
